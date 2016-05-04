abd7ec86fe5ca1a9cc24152ae37a35f3307c870b
mariabuilien <maria@iceimport.com>
2016-05-04 03:01:20 -0700

    Teacher Controller
    Beinagrind með föllum án færibreyta

--- MooseMus/Controllers/TeacherController.cs
+++ MooseMus/Controllers/TeacherController.cs
@@ -13,5 +13,86 @@ namespace MooseMus.Controllers
         {
             return View();
         }
+
+        //Kennari fer inná svæði til þess að bæta við eða breyta verkefni
+        public ActionResult addEditProject()
+        {
+            return View();
+        }
+
+        //Kennari velur að bæta við verkefni
+        public ActionResult addProject()
+        {
+            return View();
+        }
+
+        //Kennari bætir við verkefni
+        public ActionResult projectAdd()
+        {
+            return View();
+        }
+
+        //Kennari velur að breyta verkefni
+        public ActionResult editProject()
+        {
+            return View();
+        }
+
+        //Kennari velur verkefni til að breyta
+        public ActionResult selectProjectToEdit()
+        {
+            return View();
+        }
+
+        //Kennari breytir verkefni
+        public ActionResult projectEdit()
+        {
+            return View();
+        }
+
+        public ActionResult viewProjectByStudent()
+        {
+            return View();
+        }
+
+        public ActionResult selectProject()
+        {
+            return View();
+        }
+
+        public ActionResult selectSTudent()
+        {
+            return View();
+        }
+
+        public ActionResult selectProjectPart()
+        {
+            return View();
+        }
+
+        public ActionResult goBackHome()
+        {
+            return View();
+        }
+
+        public ActionResult goBackToCourses()
+        {
+            return View();
+        }
+
+        public ActionResult goBackToProjects()
+        {
+            return View();
+        }
+
+        public ActionResult goBackToStudents()
+        {
+            return View();
+        }
+
+        public ActionResult goBackToProject()
+        {
+            return View();
+        }
     }
 }
