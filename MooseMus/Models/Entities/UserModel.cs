2e8aaf042e3e36f614c53696d0dcf942503c2e2d
mariabuilien <maria@iceimport.com>
2016-05-03 22:38:26 -0700

    User model klassi
    User model klasi

---
+++ MooseMus/Models/Entities/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MooseMus.Models.Entities
+{
+    public class UserModel
+    {
+        public int userID { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
