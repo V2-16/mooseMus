c28f1a68963e3e9d48d04d26aec91c7b373ef72a
rakels13 <rakels13@ru.is>
2016-05-05 14:32:56 +0000

    changed default page

--- MooseMus/App_Start/RouteConfig.cs
+++ MooseMus/App_Start/RouteConfig.cs
@@ -14,16 +14,17 @@ namespace MooseMus
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-
-            routes.MapRoute(
                 name: "Admin-login",
                 url: "Admin/login",
                 defaults: new { controller = "Admin", action = "Index"}
                 );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "login", id = UrlParameter.Optional }
+            );
+
+            
         }
     }
 }
