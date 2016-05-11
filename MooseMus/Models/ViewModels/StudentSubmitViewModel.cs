96c42dff9afeeea7b693173b37ae58fc9c2079e2
mariabuilien <maria@iceimport.com>
2016-05-11 02:51:20 -0700

    Submit file

--- MooseMus/Models/ViewModels/StudentSubmitViewModel.cs
+++ MooseMus/Models/ViewModels/StudentSubmitViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +11,7 @@ namespace MooseMus.Models.ViewModels
         public int studentID { get; set; }
         public int projectPartID { get; set; }
         public string projectPartName { get; set; }
+        [Required(ErrorMessage = "A file upload is required")]
         public HttpPostedFileBase file { get; set; }
     }
 }
