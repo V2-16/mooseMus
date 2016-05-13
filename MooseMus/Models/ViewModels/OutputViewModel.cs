f2afaa137690a221d2e6d3844cb21cc4ab2270f0
mariabuilien <maria@iceimport.com>
2016-05-13 03:01:57 -0700

    Hægt að prófa ýmis input

---
+++ MooseMus/Models/ViewModels/OutputViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MooseMus.Models.ViewModels
+{
+    public class OutputViewModel
+    {
+        public bool accepted { get; set; }
+        public List<String> outputObtained { get; set; }
+        public List<String> outputExpected { get; set; }
+    }
+}
